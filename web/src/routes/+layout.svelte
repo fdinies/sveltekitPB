da936b85f64313f4ddf69caddd8792b436e861c5
Frank Dinies <FrankDinies@web.de>
2022-12-29 18:35:20 +0100

    User Management complete (Register, Login, Logout, Passwort Reset) with Mail Management

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 <div class="min-h-full">
 	<nav class="navbar bg-base-100 border-b">
 		<div class="flex-1">
-			<a href="/" class="btn btn-ghost normal-case text-xl">Showcase</a>
+			<a href="/" class="btn btn-ghost normal-case text-xl">SvelteKitPB</a>
 		</div>
 		<div class="flex-none">
 			{#if !data.user}
@@ -21,6 +21,7 @@
 				<div class="dropdown dropdown-end">
 					<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
 					<!-- svelte-ignore a11y-label-has-associated-control -->
+
 					<label tabindex="0" class="btn btn-ghost btn-circle avatar">
 						<div class="w-10 rounded-full">
 							<img src="https://placeimg.com/80/80/people" alt="User avatar" />
